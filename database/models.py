b7ebd46ec13083d114b3fc013f568c9dd3b29afa
Andrzej Szary <aszary@gmail.com>
2013-05-13 11:17:10 +0800

    first attribute in Pulsar model added

--- database/models.py
+++ database/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Pulsar(models.Model):
+    name =  models.CharField(max_length=200, verbose_name='Pulsar name.  The B name if exists, otherwise the J name')

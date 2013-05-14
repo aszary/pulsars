fbe722ff2d0c05867e5b86f16c375eddcf2b84a5
Andrzej Szary <aszary@gmail.com>
2013-05-14 09:46:50 +0800

    templates + static files added (metro style)

--- pulsars/views.py
+++ pulsars/views.py
@@ -1,8 +1,10 @@
 __author__ = 'aszary'
 
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def home(request):
-    output = 'Hello Pulsars!!'
-    return HttpResponse(output)
+    template = loader.get_template('pulsars/hello.xhtml')
+    c = Context()
+    return HttpResponse(template.render(c))

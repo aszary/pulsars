b368ee4d89f9a63469bc5f71141e9b36c57fcab4
Andrzej Szary <aszary@astro.ia.uz.zgora.pl>
2013-06-03 00:07:37 +0800

    serve media not only in DEBUG mode

--- pulsars/urls.py
+++ pulsars/urls.py
@@ -19,7 +19,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
-    )
+#if settings.DEBUG:
+#    urlpatterns += patterns(url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),)
